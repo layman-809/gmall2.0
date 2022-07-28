6b7367eff5b69ac4b7d290d5c5769f31efef8e36
YuMingYan <1316165298@qq.com>
2022-07-28 14:10:49 +0800

    配置nacos及启动类

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,38 +1,36 @@
 package com.atguigu.gmall.pms;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
 @EnableSwagger2
+@RefreshScope
 public class GmallPmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GmallPmsApplication.class, args);
     }
 
-    @Configuration
+   /* @Configuration
     public class MybatisPlusConfig {
         @Bean
         public PaginationInterceptor paginationInterceptor() {
             PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-            /**
+            *//**
              * 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求 默认false
              * paginationInterceptor.setOverflow(false);
              * 设置最大单页限制数量，默认 500 条，-1 不受限制
              * paginationInterceptor.setLimit(500);
              * 开启 count 的 join 优化,只针对部分 left join
-             */
+             *//*
             paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
             return paginationInterceptor;
         }
-    }
+    }*/
 
 }
